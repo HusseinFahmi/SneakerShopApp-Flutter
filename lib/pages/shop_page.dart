8ab68b9d469077c8b5e4a9d31953654d50cb499c
Hussein Fahmi <husine2022@gmail.com>
2025-07-02 12:39:43 +0300

    finish building the app.

--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:provider/provider.dart';
 import 'package:sneaker_shop_app/components/shoe_title.dart';
 import 'package:sneaker_shop_app/models/cart.dart';
@@ -25,82 +24,82 @@ class _ShopPageState extends State<ShopPage> {
         content: Text('Check your cart'),
       ),
     );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Consumer <Cart>(builder: (context, value, child) =>
-        Column(
-          children: [
-            SizedBox(height: 15),
-            Container(
-              padding: EdgeInsets.all(12),
-              margin: EdgeInsets.symmetric(horizontal: 25),
-              decoration: BoxDecoration(
-                color: Colors.grey.shade200,
-                borderRadius: BorderRadius.circular(15),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text('Search', style: TextStyle(color: Colors.grey)),
-                  Icon(Icons.search, color: Colors.grey),
-                ],
-              ),
+    return Consumer<Cart>(
+      builder: (context, value, child) => Column(
+        children: [
+          SizedBox(height: 15),
+          Container(
+            padding: EdgeInsets.all(12),
+            margin: EdgeInsets.symmetric(horizontal: 25),
+            decoration: BoxDecoration(
+              color: Colors.grey.shade200,
+              borderRadius: BorderRadius.circular(15),
             ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Search', style: TextStyle(color: Colors.grey)),
+                Icon(Icons.search, color: Colors.grey),
+              ],
+            ),
+          ),
 
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 25.0),
-              child: Text(
-                'everyone flies.. some fly longer than others',
-                style: TextStyle(color: Colors.grey.shade600),
+          Padding(
+                padding: const EdgeInsets.symmetric(vertical: 25.0),
+                child: Text(
+                  'everyone flies.. some fly longer than others',
+                  style: TextStyle(color: Colors.grey.shade600),
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Text(
-                    'Hot Picks 🔥',
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    'See all',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.blue,
-                      fontSize: 16,
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Hot Picks 🔥',
+                      style: TextStyle(
+                          fontSize: 24, fontWeight: FontWeight.bold),
                     ),
-                  ),
-                ],
+                    Text(
+                      'See all',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
 
-            SizedBox(height: 15),
+          SizedBox(height: 15),
 
-            Expanded(
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: value
-                    .getShoeList()
-                    .length,
-                itemBuilder: (context, index) {
-                  Shoe shoe = value.getShoeList()[index];
-                  return ShoeTittle(
-                      shoe: shoe, onTap: () => addShoeToCart(shoe));
-                },
+          Expanded(
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: value
+                      .getShoeList()
+                      .length,
+                  itemBuilder: (context, index) {
+                    Shoe shoe = value.getShoeList()[index];
+                    return ShoeTittle(
+                        shoe: shoe, onTap: () => addShoeToCart(shoe));
+                  },
+                ),
               ),
-            ),
 
-            Padding(
-              padding: const EdgeInsets.only(
-                  top: 25.0, left: 25.0, right: 25.0),
-              child: Divider(
-                color: Colors.grey.shade300,
-              ),
-            ),
-          ],
-        )
-      ,);
+          Padding(
+            padding: const EdgeInsets.only(top: 25.0, left: 25.0, right: 25.0),
+            child: Divider(color: Colors.grey.shade300),
+          ),
+        ],
+      ),
+    );
   }
 }
